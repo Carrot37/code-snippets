f0c27a393727926db0968128d8ff575b40ee0303
Carrot37 <npruitt.de@mysummitps.org>
2015-12-29 17:43:22 -0800

    Create python-snippets-basic.py

---
+++ python-snippets-basic.py
@@ -0,0 +1,28 @@
+print "Hello, World!" # Prints "Hello, World!" to the console
+
+input1 = raw_input() # Asks for user input
+print input1 # Prints the user's input
+
+print 1 + 1 # Basic math in Python
+print 2 - 1
+print 3 * 3
+print 3 / 2 # Returns an int
+print 3 / 2.0 # Returns a float
+
+print str(5) # Converts int to string
+print int("5") # Converts string to int
+
+# List of basic number operators: + - * / % > < >= <= == !=
+
+if 1 + 1 == 2:
+	print "1+1=2" # Basic if statment
+else:
+	print "1+1!=2" # Basic else statement
+	
+for i in range(0, 6):
+	print "Hi!" # Repeats 5 times
+	
+# INCOMPLETE SECTION
+[1, 2, 3] # Example list
+#SECTION END--------
+
